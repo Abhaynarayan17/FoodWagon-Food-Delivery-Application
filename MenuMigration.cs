a399837aeb8cf7bb91f01013b64b6a99487025a9
Abhaynarayan A <132026263+Abhaynarayan17@users.noreply.github.com>
2024-10-03 23:08:25 +0530

    Create MenuMigration.cs

---
+++ MenuMigration.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodWagon.Migrations.Menu
+{
+    /// <inheritdoc />
+    public partial class Migrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FoodId = table.Column<int>(type: "int", nullable: false),
+                    EmailId = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    FoodName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    FoodPrice = table.Column<decimal>(type: "decimal(16,2)", precision: 16, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Menus");
+        }
+    }
+}
