0fefd662d6609ff6eb8c883f6015829668bae93a
Abhaynarayan A <132026263+Abhaynarayan17@users.noreply.github.com>
2024-10-03 23:44:59 +0530

    Create customer-signup-details.model.ts

---
+++ customer-signup-details.model.ts
@@ -0,0 +1,12 @@
+export class CustomerSignupDetails {
+    customerID: string ="";
+    customerName: string ="";
+    emailID: string ="";
+    password: string ="";
+    confirmPassword: string ="";
+    phoneNumber: string ="";
+    customerAddress: string ="";
+    city: string ="";
+    state: string ="";
+    pincode: string ="";
+}
