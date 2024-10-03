64d2d41f5569878a742abbaae09b3235782e38d4
Abhaynarayan A <132026263+Abhaynarayan17@users.noreply.github.com>
2024-10-03 23:43:50 +0530

    Create registerdetails-model.ts

---
+++ registerdetails-model.ts
@@ -0,0 +1,13 @@
+export class RegisterDetails {      
+    restaurantName: string= "";
+    emailID: string = "";
+    sellerName: string="";
+    password: string= "";
+    confirmPassword: string= "";
+    restaurantAddress: string= "";
+    phoneNumber: string="";
+    city: string= "";
+    state: string= "";
+    pincode: string= "";
+    
+}
