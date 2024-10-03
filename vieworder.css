85f1d9a951a7e21a6364c88e547e3c21bee5c97a
Abhaynarayan A <132026263+Abhaynarayan17@users.noreply.github.com>
2024-10-03 23:34:39 +0530

    Create vieworder.css

---
+++ vieworder.css
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vieworder',
+  standalone: true,
+  imports: [],
+  templateUrl: './vieworder.component.html',
+  styleUrl: './vieworder.component.css'
+})
+export class VieworderComponent {
+
+}
