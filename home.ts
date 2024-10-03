3330bb1d7f38490023367a7054bd1c2b6634074d
Abhaynarayan A <132026263+Abhaynarayan17@users.noreply.github.com>
2024-10-03 23:18:59 +0530

    Create home.ts

---
+++ home.ts
@@ -0,0 +1,14 @@
+import { Component, } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  constructor() { }
+
+}
